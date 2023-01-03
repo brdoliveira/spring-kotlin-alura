bd9c5ee5c064cbddb1e792dcaf9342a60357f3c6
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-02 23:00:35 -0300

    FEAT(Curso): Criando classe Curso

---
+++ src/main/kotlin/br/com/alura/forum/model/Curso.kt
@@ -0,0 +1,7 @@
+package br.com.alura.forum.model
+
+data class Curso(
+    val id: Long,
+    val nome: String,
+    val categoria: String
+)
---
+++ src/main/kotlin/br/com/alura/forum/model/Topico.kt
@@ -0,0 +1,3 @@
+package br.com.alura.forum.model
+
+data class Topico()
