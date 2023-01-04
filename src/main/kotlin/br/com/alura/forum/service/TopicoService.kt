5a823e8227e392da5567e9c9a681d64420cd2d93
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-03 23:45:32 -0300

    FEAT(TopicoService): Cirando classe TopicoService

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,4 +1,73 @@
 package br.com.alura.forum.service
 
-class TopicoService {
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TopicoService (private var topicos: List<Topico>){
+    init {
+        val topico1 = Topico(
+            id=1,
+            titulo = "Duvido kt",
+            mensagem = "Variaveis kt",
+            curso = Curso(
+                id = 1,
+                nome = "kt",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana S.",
+                email = "a.aa@gmail.com"
+            )
+        )
+
+        val topico2 = Topico(
+            id=2,
+            titulo = "Duvido kt",
+            mensagem = "Variaveis kt",
+            curso = Curso(
+                id = 1,
+                nome = "kt",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana S.",
+                email = "a.aa@gmail.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id=2,
+            titulo = "Duvido kt",
+            mensagem = "Variaveis kt",
+            curso = Curso(
+                id = 1,
+                nome = "kt",
+                categoria = "Programacao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana S.",
+                email = "a.aa@gmail.com"
+            )
+        )
+
+        topicos = Arrays.asList(topico1,topico2,topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+    }
+
 }
