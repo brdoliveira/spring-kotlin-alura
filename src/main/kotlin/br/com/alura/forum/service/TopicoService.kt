a670eef9b5aad4af988e56efde109e7ec56dc0d2
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-04 20:01:27 -0300

    FEAT(TopicoService): Criando o método de cadastrar

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -5,60 +5,10 @@ import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 import java.util.*
+import kotlin.collections.ArrayList
 
 @Service
-class TopicoService (private var topicos: List<Topico>){
-    init {
-        val topico1 = Topico(
-            id=1,
-            titulo = "Duvido kt",
-            mensagem = "Variaveis kt",
-            curso = Curso(
-                id = 1,
-                nome = "kt",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana S.",
-                email = "a.aa@gmail.com"
-            )
-        )
-
-        val topico2 = Topico(
-            id=2,
-            titulo = "Duvido kt",
-            mensagem = "Variaveis kt",
-            curso = Curso(
-                id = 1,
-                nome = "kt",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana S.",
-                email = "a.aa@gmail.com"
-            )
-        )
-
-        val topico3 = Topico(
-            id=2,
-            titulo = "Duvido kt",
-            mensagem = "Variaveis kt",
-            curso = Curso(
-                id = 1,
-                nome = "kt",
-                categoria = "Programacao"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana S.",
-                email = "a.aa@gmail.com"
-            )
-        )
-
-        topicos = Arrays.asList(topico1,topico2,topico3)
-    }
+class TopicoService (private var topicos: List<Topico> = ArrayList()){
 
     fun listar(): List<Topico> {
         return topicos
@@ -68,6 +18,10 @@ class TopicoService (private var topicos: List<Topico>){
         return topicos.stream().filter { t ->
             t.id == id
         }.findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
     }
 
 }
