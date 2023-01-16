2d0eb6bad58b1c31c9774de4251c2d0deba0bbf8
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-15 23:57:57 -0300

    FEAT(TopicoController): Melhorando resposta do Spring

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -4,7 +4,10 @@ import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.util.UriBuilder
+import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
@@ -18,10 +21,20 @@ class TopicoController(private val service: TopicoService) {
     fun buscarPorId(@PathVariable id: Long): TopicoView = service.buscarPorId(id)
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) = service.cadastrar(form)
+    fun cadastrar(
+        @RequestBody @Valid form: NovoTopicoForm,
+        uriBuilder: UriComponentsBuilder
+    ): ResponseEntity<TopicoView> {
+        val topicoView = service.cadastrar(form)
+        val uri = uriBuilder.path("/topicos/${topicoView.id}").build().toUri()
+        return ResponseEntity.created(uri).body(topicoView)
+    }
 
     @PutMapping
-    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) = service.atualizar(form)
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm): ResponseEntity<TopicoView>{
+        val topicoView = service.atualizar(form)
+        return ResponseEntity.ok(topicoView)
+    }
 
     @DeleteMapping("/{id}")
     fun deletar(@PathVariable id:Long) =  service.deletar(id)
