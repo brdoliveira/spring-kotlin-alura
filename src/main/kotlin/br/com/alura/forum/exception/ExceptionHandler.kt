348c7279cde1720e00ea86d38391987aac56aa7a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-16 23:52:11 -0300

    FEAT(ExceptionHandler): Criando classe ExceptionHandler

--- src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -1,4 +1,39 @@
 package br.com.alura.forum.exception
 
+import br.com.alura.forum.dto.ErrorView
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
+
+@RestControllerAdvice
 class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFound(
+        exception: NotFoundException,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.NOT_FOUND.value(),
+            error = HttpStatus.NOT_FOUND.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleServerError(
+        exception: Exception,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
 }
