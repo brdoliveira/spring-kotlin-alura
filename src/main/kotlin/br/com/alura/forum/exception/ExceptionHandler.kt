b778c17d3d1088b3f264e73325fe5656429de072
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-17 23:24:46 -0300

    FEAT(ExceptionHanler): Adicionando BadRequestException

--- src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.exception
 
 import br.com.alura.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.servlet.http.HttpServletRequest
@@ -22,6 +23,25 @@ class ExceptionHandler {
             path = request.servletPath
         )
     }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach{
+                e -> errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
+        )
+    }
+
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
