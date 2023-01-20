7e3d18dcdeb1bf3b24c4fbbc3e8ab79029970480
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-20 19:06:58 -0300

    FEAT(UsuarioService): Criando conexão com o bd

--- src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
+++ src/main/kotlin/br/com/alura/forum/service/UsuarioService.kt
@@ -1,22 +1,13 @@
 package br.com.alura.forum.service
 
 import br.com.alura.forum.model.Usuario
+import br.com.alura.forum.repository.UsuarioRepository
 import org.springframework.stereotype.Service
 
 @Service
-class UsuarioService(var usuarios: List<Usuario>) {
-    init{
-        val usuario = Usuario(
-            id=1,
-            nome="Ana da Silva",
-            email = "ana@email.com.br"
-        )
-        usuarios = listOf(usuario)
-    }
+class UsuarioService(private val repository: UsuarioRepository) {
 
     fun buscarPorId(id:Long): Usuario {
-        return usuarios.stream().filter{
-                c -> c.id == id
-        }.findFirst().get()
+        return repository.getOne(id)
     }
 }
