7d90c035e82f9b739b9dfba5d4431e57e513978c
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-20 19:05:36 -0300

    FEAT(CursoRepository): Criando interface repository CursoRepositroy

--- src/main/kotlin/br/com/alura/forum/repository/CursoRepository.kt
+++ src/main/kotlin/br/com/alura/forum/repository/CursoRepository.kt
@@ -1,4 +1,7 @@
 package br.com.alura.forum.repository
 
-interface CursoRepositroy {
+import br.com.alura.forum.model.Curso
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CursoRepository : JpaRepository<Curso,Long> {
 }
