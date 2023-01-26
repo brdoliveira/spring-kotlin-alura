008efbc2740442e271f4c40fffab1d9065344abf
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-25 22:41:39 -0300

    FEAT(V1__create_table_curso): Adicionando flag de not null e auto_increment

--- src/main/resources/db/migration/V1__create_table_curso.sql
+++ src/main/resources/db/migration/V1__create_table_curso.sql
@@ -1,8 +1,8 @@
 create table curso(
-    id bigint not null,
-    nome varchar(50),
-    categoria varchar(50),
+    id bigint not null auto_increment,
+    nome varchar(50) not null,
+    categoria varchar(50) not null,
     primary key(id)
 );
 
-insert into curso values(1,'Kotlin','Programacao')
+insert into curso values(1,'Kotlin','Programacao');
