d5f3e784fe412bf03bddc7dac1b29cbe29c31686
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-01-25 23:15:15 -0300

    FEAT(TopicoRepository):  Adicionando findByCursoNome

--- src/main/kotlin/br/com/alura/forum/repository/TopicoRepository.kt
+++ src/main/kotlin/br/com/alura/forum/repository/TopicoRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface TopicoRepository: JpaRepository<Topico,Long> {
 
+    fun findByCursoNome(nomeCurso: String): List<Topico>
 }
