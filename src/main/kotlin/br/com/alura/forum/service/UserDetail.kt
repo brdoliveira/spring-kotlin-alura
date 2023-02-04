82487ab626638af8ad4c4884c04acf6f9c5b723d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-02-04 20:53:38 -0300

    FEAT(UserDetail): Criando classe para autenticacao

---
+++ src/main/kotlin/br/com/alura/forum/service/UserDetail.kt
@@ -0,0 +1,22 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Usuario
+import org.springframework.security.core.userdetails.UserDetails
+
+class UserDetail(
+    private val usuario: Usuario
+): UserDetails {
+    override fun getAuthorities() = null
+
+    override fun getPassword(): String = usuario.password
+
+    override fun getUsername(): String = usuario.email
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
