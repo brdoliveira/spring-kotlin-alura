55dbbb1e60a2b295fdab628930726b33582dacaf
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-02-18 22:55:26 -0300

    FEAT(V9__insert_topico.sql): Inserindo topico

---
+++ src/main/resources/db/migration/V9__insert_topico.sql
@@ -0,0 +1,3 @@
+INSERT INTO topico(id,titulo,mensagem,data_criacao,status,curso_id,autor_id)
+VALUES(1,'Duvida sobre kotlin','Minha função let nao funciona',
+       '2021-12-24 12:00:00','NAO_RESPONDIDO',1,1)
