1b3c19e5dbd0bbf3e4e7c4f129aa17f75e1af6e1
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-02-26 23:57:19 -0300

    FEAT(RespostaService): Criando classe RespostaService

---
+++ src/main/kotlin/br/com/alura/forum/service/RespostaService.kt
@@ -0,0 +1,20 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Resposta
+import br.com.alura.forum.repository.RespostaRepository
+import org.springframework.stereotype.Service
+
+@Service
+class RespostaService(
+    private val respostaRepository: RespostaRepository,
+    private val emailService: EmailService
+){
+
+    fun salvar(resposta: Resposta){
+        respostaRepository.save(resposta)
+
+        val autor = resposta.topico.autor.email
+
+        emailService.notificar(autor)
+    }
+}
